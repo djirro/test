f80866a845653cf076ba420d8ae6ef01f2bec1f6
Egor Danshov <djirrochannel@gmail.com>
2021-07-16 12:54:06 -0700

    First commit
    test only

---
+++ Shared/testApp.swift
@@ -0,0 +1,18 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by Егор Даншов on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
